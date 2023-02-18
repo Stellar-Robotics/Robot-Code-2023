64439ff3b4f5233f33f8dee784cefcc24843b328
5413-Laptop-1 <zortanproductions@gmail.com>
2023-02-17 19:49:31 -0500

    Cleaned up code and made a guitar controllable arm

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -3,7 +3,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 
 import com.revrobotics.CANSparkMax;
-//import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class DriveTrain {
@@ -12,22 +11,18 @@ public class DriveTrain {
         NEW
     }
 
-    //private final DriveLayo currentDrive = Drive.CYGNUS;
-
     public final CANSparkMax driveLeftFront;
     public final CANSparkMax driveRightFront;
     public final CANSparkMax driveLeftRear;
     public final CANSparkMax driveRightRear;
+
     // Define Controllers
+
     private final Joystick driverLeft = new Joystick(0);
     private final Joystick driverRight = new Joystick(1);
-    //private final Joystick Operator = new Joystick(2);
   
     double driveRight = 0;
     double driveLeft = 0;
-  
-    //boolean toggleState = false;
-    //boolean toggleLast = false; 
 
     public DriveTrain(DriveLayout layout ) {
         if (layout == DriveLayout.NEW) {
@@ -47,12 +42,11 @@ public class DriveTrain {
 
         double driveRight = 0;
         double driveLeft = 0;
-      
-        //boolean toggleState = false;
-        //boolean toggleLast = false;
 
-                // A small state machine to toggle different drive train speeds.
+        // A small state machine to toggle different drive train speeds.
+
         double driveSpeedMultiplier = toggleState?0.2:1;
+
         if (toggleState) {
             if (driverRight.getRawButton(1)) {
                 driveLeft = driverRight.getZ() * driveSpeedMultiplier * 0.8;
@@ -73,15 +67,9 @@ public class DriveTrain {
                 driveRight = driverRight.getY() * driveSpeedMultiplier;
             }
         }
-        /*
-        if ( (driverLeft.getRawButton(12) == true) & (!toggleLast) ) {
-        toggleState  = !toggleState;
-        }
-
-        toggleLast = driverLeft.getRawButton(12);
-        */
 
         // Applying the drive train speed using the raw axis input multiplied by the toggle fraction.
+        
         driveLeftFront.set(driveLeft);
         driveRightFront.set(driveRight);
         driveLeftRear.set(driveLeft);
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,32 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+// Import necessary libraries
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.ADIS16448_IMU.IMUAxis;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Servo;
-
-import java.util.Random;
-
-//import javax.security.auth.x500.X500Principal;
-
-// importing some motors
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMax.ControlType;
-//import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.SparkMaxAlternateEncoder;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -37,73 +26,77 @@ import com.revrobotics.SparkMaxAlternateEncoder;
  */
 
 public class Robot extends TimedRobot {
+
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  // Declare arm motor
-  //private final CANSparkMax armMotor = new CANSparkMax(34, MotorType.kBrushless);
-
   private final DriveTrain driveTrain = new DriveTrain(DriveTrain.DriveLayout.NEW);
+
+  // Declare controll devices.
+
+  private final Joystick driverLeft = new Joystick(0);
+  private final Joystick operator = new Joystick(2);
+  private final Joystick guitar = new Joystick(3);
+
+  // Setting variables for logic.
 
   double driveRight = 0;
   double driveLeft = 0;
 
   double pitchIAccumulator = 0;
 
-  private final Joystick driverLeft = new Joystick(0);
-  private final Joystick operator = new Joystick(2);
-  private final Joystick guitar = new Joystick(3);
-
-
   boolean toggleState = false;
   boolean toggleLast = false; 
 
-  // Declare Encoder
-  ADIS16470_IMU gyro = new ADIS16470_IMU();
+  double targetPosition = 0;
 
-  CANSparkMax arm = new CANSparkMax(34, MotorType.kBrushless);
+  // Actuator and Gyro Declaration
 
-  RelativeEncoder armEncoder = arm.getEncoder();
+  ADIS16470_IMU gyro = new ADIS16470_IMU(); // Creating the gyro refrence.
 
-  SparkMaxPIDController armPIDController = arm.getPIDController();
+  CANSparkMax arm = new CANSparkMax(34, MotorType.kBrushless); // Creating the Arm Motor refrence.
 
-  
+  RelativeEncoder armEncoder = arm.getEncoder(); // Getting the encoder for the arm
 
-  
-  
+  SparkMaxPIDController armPIDController = arm.getPIDController(); // Creating a PID controller.
 
-  // Decalring the lighting module.
-
-  Spark lightController = new Spark(0);
+  Spark lightController = new Spark(0); // Decalring the lighting module.
 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
 
-   // set arm motor run mode
-   //SparkMaxPIDController setEncoder = armMotor.getPIDController();
+  // Initialization code to be run after robot startup
+
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    Pneumatic.compressor.disable();
-    lightController.set(0.69);
+
+    // Declaring telemetry for SmartDashboard
         
     SmartDashboard.putNumber("Pitch", 0);
     SmartDashboard.putNumber("Yaw", 0);
     SmartDashboard.putNumber("Pitch Velocity", 0);
-    
     SmartDashboard.putNumber("P", 0.0045);
     SmartDashboard.putNumber("I", 0.00001);
     SmartDashboard.putNumber("D", 0.006);
     SmartDashboard.putNumber("PitchMaxDVelocity", 2.5);
     SmartDashboard.putNumber("StopAngle", 4);
-    SmartDashboard.putNumber("ArmP", 0);
+
+    SmartDashboard.putNumber("ArmP", 0.01);
+    SmartDashboard.putNumber("ArmI", 0); 
+    SmartDashboard.putNumber("ArmD", 0); 
     SmartDashboard.putNumber("ArmPos", 0);
+
+    // Setting hardware device values
+
+    Pneumatic.compressor.disable();
+    lightController.set(0.69);
 
   }
 
@@ -151,25 +144,24 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-public void teleopInit() { Pneumatic.compressor.enableDigital();  /*Pneumatic.compressor.disable();*/ }
+  public void teleopInit() { Pneumatic.compressor.enableDigital(); }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
 
     // Calls the state machine in the DriveTrain class
+
     if ( (driverLeft.getRawButton(12) == true) & (!toggleLast) ) {
+
       toggleState  = !toggleState;
+
     }
 
     toggleLast = driverLeft.getRawButton(12);
     driveTrain.tankDrive(toggleState);
 
-    // Getting the arm up and going
-
-    arm.set(operator.getY());
-
-    SmartDashboard.putNumber("Data", operator.getY());
+    // Pneumatic Actuation Code
 
     Pneumatic.Solenoid.set(guitar.getRawButton(1));
 
@@ -177,18 +169,35 @@ public void teleopInit() { Pneumatic.compressor.enableDigital();  /*Pneumatic.co
 
     lightController.set(0.37);
 
-    SmartDashboard.putNumber("Encoder", armEncoder.getPosition());
+    // Arm Positioning and PID
 
-    armPIDController.setReference(SmartDashboard.getNumber("ArmPos", 0), CANSparkMax.ControlType.kPosition);
+    if ( guitar.getPOV() == 0 & targetPosition <= 70 ) {
 
-    double armP = SmartDashboard.getNumber("ArmP", 0);
-    double armI = 0;
-    double armD = 0;
+      targetPosition += 0.5;
+
+    }
+
+    else if ( guitar.getPOV() == 180 & targetPosition >= 0 ) {
+
+      targetPosition -=0.5;
+
+    }
+
+    armPIDController.setReference(targetPosition, CANSparkMax.ControlType.kPosition);
+
+    double armP = SmartDashboard.getNumber("ArmP", 0.02);
+    double armI = SmartDashboard.getNumber("ArmI", 0.001);
+    double armD = SmartDashboard.getNumber("ArmD", 0.02);
 
     armPIDController.setP(armP);
     armPIDController.setI(armI);
     armPIDController.setD(armD);
 
+    // Update SmartDashboard
+
+    SmartDashboard.putNumber("Encoder", armEncoder.getPosition());
+    SmartDashboard.putNumber("GuitarHat", guitar.getPOV());
+    SmartDashboard.putNumber("OperatorY", operator.getY());
 
   }
 
@@ -236,7 +245,7 @@ public void disabledInit() { Pneumatic.compressor.disable(); }
     double pitchP = SmartDashboard.getNumber("P", 0);
     double pitchI = SmartDashboard.getNumber("I", 0);
 
-    double stopAngle = SmartDashboard.getNumber("StopAngle", 4);
+    //double stopAngle = SmartDashboard.getNumber("StopAngle", 4);
     double pitchMaxDVelocity = SmartDashboard.getNumber("PitchMaxDVelocity", 100);
     
     
